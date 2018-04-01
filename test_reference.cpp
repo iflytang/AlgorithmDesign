03aa8367784f6983bcfed1e56572e979c1c34f0d
iflytang <iflytang@qq.com>
2018-04-01 21:12:06 +0800

    test reference parameter and pointer parameter

--- test_reference.cpp
+++ test_reference.cpp
@@ -15,10 +15,21 @@ int swap(int & a, int & b) {  // reference as parameter
     return  0;
 }
 
+int swap2(int * a, int * b) {
+    int * tmp;
+    tmp = a;
+    a = b;
+    b = tmp;
+
+    return 0;
+}
+
 int main() {
     int n1 = 100;
     int n2 = 50;
     cout << "before swap -> n1:" << n1 << ", n2:" << n2 << endl;
     swap(n1, n2);
     cout << "after swap -> n1:" << n1 << ", n2:" << n2 << endl;
+    swap2(&n1, &n2);
+    cout << "after swap2 -> n1:" << n1 << ", n2:" << n2 << endl;
 }
