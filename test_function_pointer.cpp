7d9730f19b347fc70e9e832c50ac470d872fe18c
iflytang <iflytang@qq.com>
2018-04-22 09:46:03 +0800

    test function pointer as callback function

---
+++ test_function_pointer.cpp
@@ -0,0 +1,44 @@
+//
+// Created by tsf on 18-4-21.
+//
+
+
+#include <iostream>
+
+using namespace std;
+
+typedef int (*Calculate) (int, int); // define a function pointer type
+
+int plus_(int x, int y) {
+    return x+y;
+}
+
+int minus_(int x, int y) {
+    return x-y;
+}
+
+int time_(int x, int y) {
+    return x*y;
+}
+
+int divide_(int x, int y) {
+    return x/y;
+}
+
+int call_back(Calculate calculate, int x, int y) {   // call back function
+    int value = calculate(x, y);  // execute the function pointer with its arguments
+    cout << "value: " << value << endl;
+}
+
+int main() {
+    cout << "please input two nums:";
+    int x, y;
+    cin >> x >> y;
+
+    call_back(plus_, x, y);  // const function pointer as parameter to call back
+    call_back(minus_, x, y);
+    call_back(time_, x, y);
+    call_back(divide_, x, y);
+
+    return 0;
+}
