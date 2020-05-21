f0845850d55e464c81dcd7d88bc6c0c6bd31d110
iflytan <iflytang@qq.com>
2020-05-21 09:29:03 +0800

    parse INT packers correctly

--- WiresharkLuaParser/INT_PKT_Lua_5_2.lua
+++ WiresharkLuaParser/INT_PKT_Lua_5_2.lua
@@ -58,15 +58,15 @@ do
     local int_d_in_port = ProtoField.uint32("INT.in_port", "in_port", base.DEC)
     local int_d_out_port = ProtoField.uint32("INT.out_port", "out_port", base.DEC)
 
-    local int_d_ingress_time = ProtoField.uint64("INT.ingress_time", "ingress_time", base.DEC)
-    local int_d_ingress_time_ovs_pof = ProtoField.uint64("INT.ingress_time", "ingress_time (us)", base.DEC)
-    local int_d_ingress_time_tofino = ProtoField.uint64("INT.ingress_time", "ingress_time (ns)", base.DEC)
+    local int_d_ingress_time = ProtoField.uint64("INT.ingress_time", "ingress_time", base.HEX)
+    --local int_d_ingress_time_ovs_pof = ProtoField.uint64("INT.ingress_time", "ingress_time (us)", base.DEC)
+    --local int_d_ingress_time_tofino = ProtoField.uint64("INT.ingress_time", "ingress_time (ns)", base.DEC)
 
     local int_d_hop_latency = ProtoField.uint32("INT.hop_latency", "hop_latency", base.DEC)
     local int_d_hop_latency_ovs_pof = ProtoField.uint32("INT.hop_latency", "hop_latency (us)", base.DEC)
     local int_d_hop_latency_tofino = ProtoField.uint32("INT.hop_latency", "hop_latency (ns)", base.DEC)
 
-    local int_d_bandwidth = ProtoField.float("INT.bandwidth", "bandwidth (Gbps)", base.DEC)
+    local int_d_bandwidth = ProtoField.float("INT.bandwidth", "bandwidth (Mbps)", base.FLOAT)
     local int_d_n_packets = ProtoField.uint64("INT.n_packets", "n_packets", base.DEC)
     local int_d_n_bytes = ProtoField.uint64("INT.n_bytes", "n_bytes", base.DEC)
     local int_d_queue = ProtoField.uint32("INT.queue", "queue", base.DEC)        -- not supported by ovs-pof
@@ -86,8 +86,8 @@ do
         int_d_out_port,
 
         int_d_ingress_time,
-        int_d_ingress_time_ovs_pof,
-        int_d_ingress_time_tofino,
+        --int_d_ingress_time_ovs_pof,
+        --int_d_ingress_time_tofino,
 
         int_d_hop_latency,
         int_d_hop_latency_ovs_pof,
@@ -104,7 +104,8 @@ do
 
     local function dissector(buf, pkt, root)
         local base = INT_h_base
-        local bit_one = 0x01
+        local byte_one = 0x01
+        local byte_zero = 0x00
         local switch_mask = 0xff000000
 
         local rt = root:add(INT_proto, buf)
@@ -114,9 +115,9 @@ do
         base = base + INT_h_type_len
 
         -- check int type
-        --if v_int_h_type:uint() ~= INT_type_val then
-        --    return false
-        --end
+        if v_int_h_type:uint() ~= INT_type_val then
+            return false
+        end
         rt:add(int_h_type, v_int_h_type)
 
         pkt.cols.protocol:set("INT Protocol")
@@ -134,7 +135,7 @@ do
 
 
         local v_int_d_dpid = 0
-        if (bit32.band(v_int_h_mapInfo:uint(), bit32.lshift(bit_one, 0))) then      -- must contain device_id
+        if (bit32.band(v_int_h_mapInfo:uint(), bit32.lshift(byte_one, 0))) then      -- must contain device_id
             v_int_d_dpid = buf(base, INT_d_dpid_len)
             base = base + INT_d_dpid_len
         else
@@ -148,60 +149,60 @@ do
             t:set_text("Hop " .. i)
 
             -- ovs-pof and tofino, distinguished by device_id
-            if (bit32.band(v_int_d_dpid:uint(), switch_mask) == 0x00) then   -- ovs-pof
+            if (bit32.band(v_int_d_dpid:uint(), switch_mask) == byte_zero) then   -- ovs-pof
                 local switch_mapInfo = bit32.band(v_int_h_mapInfo:uint(), CPU_BASED_MAPINFO)
 
-                t:add(int_d_dpid_ovs_pof, v_int_d_dpid)
+                t:add(int_d_dpid, v_int_d_dpid)
 
-                if (bit32.band(switch_mapInfo, bit32.lshift(bit_one, 1))) then
+                if (bit32.band(switch_mapInfo, bit32.lshift(byte_one, 1)) ~= byte_zero) then
                     local v_int_d_in_port = buf(base, INT_d_in_port_len)
                     t:add(int_d_in_port, v_int_d_in_port)
                     base = base + INT_d_in_port_len
                 end
 
-                if (bit32.band(switch_mapInfo, bit32.lshift(bit_one, 2))) then
+                if (bit32.band(switch_mapInfo, bit32.lshift(byte_one, 2)) ~= byte_zero) then
                     local v_int_d_out_port = buf(base, INT_d_out_port_len)
                     t:add(int_d_out_port, v_int_d_out_port)
                     base = base + INT_d_out_port_len
                 end
 
-                if (bit32.band(switch_mapInfo, bit32.lshift(bit_one, 3))) then
+                if (bit32.band(switch_mapInfo, bit32.lshift(byte_one, 3)) ~= byte_zero) then
                     local v_int_d_ingress_time = buf(base, INT_d_ingress_time_len)
-                    t:add(int_d_ingress_time_ovs_pof, v_int_d_ingress_time)
+                    t:add(int_d_ingress_time, v_int_d_ingress_time)
                     base = base + INT_d_ingress_time_len
                 end
 
-                if (bit32.band(switch_mapInfo, bit32.lshift(bit_one, 4))) then
+                if (bit32.band(switch_mapInfo, bit32.lshift(byte_one, 4)) ~= byte_zero) then
                     local v_int_d_hop_latency = buf(base, INT_d_hop_latency_len)
                     t:add(int_d_hop_latency_ovs_pof, v_int_d_hop_latency)
                     base = base + INT_d_hop_latency_len
                 end
 
-                if (bit32.band(switch_mapInfo, bit32.lshift(bit_one, 5))) then
-                    local v_int_d_bandwidth = buf(base, INT_d_bandwidth_len)
+                if (bit32.band(switch_mapInfo, bit32.lshift(byte_one, 5)) ~= byte_zero) then
+                    local v_int_d_bandwidth = buf(base, INT_d_bandwidth_len):le_float()
                     t:add(int_d_bandwidth, v_int_d_bandwidth)
                     base = base + INT_d_bandwidth_len
                 end
 
-                if (bit32.band(switch_mapInfo, bit32.lshift(bit_one, 6))) then
+                if (bit32.band(switch_mapInfo, bit32.lshift(byte_one, 6)) ~= byte_zero) then
                     local v_int_d_n_packets = buf(base, INT_d_n_packets_len)
                     t:add(int_d_n_packets, v_int_d_n_packets)
                     base = base + INT_d_n_packets_len
                 end
 
-                if (bit32.band(switch_mapInfo, bit32.lshift(bit_one, 7))) then
+                if (bit32.band(switch_mapInfo, bit32.lshift(byte_one, 7)) ~= byte_zero) then
                     local v_int_d_n_bytes = buf(base, INT_d_n_bytes_len)
                     t:add(int_d_n_bytes, v_int_d_n_bytes)
                     base = base + INT_d_n_bytes_len
                 end
 
-                if (bit32.band(switch_mapInfo, bit32.lshift(bit_one, 8))) then    -- not supported by ovs-pof
+                if (bit32.band(switch_mapInfo, bit32.lshift(byte_one, 8)) ~= byte_zero) then    -- not supported by ovs-pof
                     local v_int_d_queue = buf(base, INT_d_queue_len)
                     t:add(int_d_queue, v_int_d_queue)
                     base = base + INT_d_queue_len
                 end
 
-                if (bit32.band(switch_mapInfo, bit32.lshift(bit_one, 9))) then
+                if (bit32.band(switch_mapInfo, bit32.lshift(byte_one, 9)) ~= byte_zero) then
                     local v_int_d_fwd_acts = buf(base, INT_d_fwd_acts_len)
                     t:add(int_d_fwd_acts, v_int_d_fwd_acts)
                     base = base + INT_d_fwd_acts_len
@@ -211,55 +212,55 @@ do
 
                 t:add(int_d_dpid_tofino, v_int_d_dpid)
 
-                if (bit32.band(switch_mapInfo, bit32.lshift(bit_one, 1))) then
+                if (bit32.band(switch_mapInfo, bit32.lshift(byte_one, 1)) ~= byte_zero) then
                     local v_int_d_in_port = buf(base, INT_d_in_port_len)
                     t:add(int_d_in_port, v_int_d_in_port)
                     base = base + INT_d_in_port_len
                 end
 
-                if (bit32.band(switch_mapInfo, bit32.lshift(bit_one, 2))) then
+                if (bit32.band(switch_mapInfo, bit32.lshift(byte_one, 2)) ~= byte_zero) then
                     local v_int_d_out_port = buf(base, INT_d_out_port_len)
                     t:add(int_d_out_port, v_int_d_out_port)
                     base = base + INT_d_out_port_len
                 end
 
-                if (bit32.band(switch_mapInfo, bit32.lshift(bit_one, 3))) then
+                if (bit32.band(switch_mapInfo, bit32.lshift(byte_one, 3)) ~= byte_zero) then
                     local v_int_d_ingress_time = buf(base, INT_d_ingress_time_len)
-                    t:add(int_d_ingress_time_tofino, v_int_d_ingress_time)
+                    t:add(int_d_ingress_time, v_int_d_ingress_time)
                     base = base + INT_d_ingress_time_len
                 end
 
-                if (bit32.band(switch_mapInfo, bit32.lshift(bit_one, 4))) then
+                if (bit32.band(switch_mapInfo, bit32.lshift(byte_one, 4)) ~= byte_zero) then
                     local v_int_d_hop_latency = buf(base, INT_d_hop_latency_len)
                     t:add(int_d_hop_latency_tofino, v_int_d_hop_latency)
                     base = base + INT_d_hop_latency_len
                 end
 
-                if (bit32.band(switch_mapInfo, bit32.lshift(bit_one, 5))) then
-                    local v_int_d_bandwidth = buf(base, INT_d_bandwidth_len)
+                if (bit32.band(switch_mapInfo, bit32.lshift(byte_one, 5)) ~= byte_zero) then
+                    local v_int_d_bandwidth = buf(base, INT_d_bandwidth_len):le_float()
                     t:add(int_d_bandwidth, v_int_d_bandwidth)
                     base = base + INT_d_bandwidth_len
                 end
 
-                if (bit32.band(switch_mapInfo, bit32.lshift(bit_one, 6))) then
+                if (bit32.band(switch_mapInfo, bit32.lshift(byte_one, 6)) ~= byte_zero) then
                     local v_int_d_n_packets = buf(base, INT_d_n_packets_len)
                     t:add(int_d_n_packets, v_int_d_n_packets)
                     base = base + INT_d_n_packets_len
                 end
 
-                if (bit32.band(switch_mapInfo, bit32.lshift(bit_one, 7))) then
+                if (bit32.band(switch_mapInfo, bit32.lshift(byte_one, 7)) ~= byte_zero) then
                     local v_int_d_n_bytes = buf(base, INT_d_n_bytes_len)
                     t:add(int_d_n_bytes, v_int_d_n_bytes)
                     base = base + INT_d_n_bytes_len
                 end
 
-                if (bit32.band(switch_mapInfo, bit32.lshift(bit_one, 8))) then    -- not supported by ovs-pof
+                if (bit32.band(switch_mapInfo, bit32.lshift(byte_one, 8)) ~= byte_zero) then    -- not supported by ovs-pof
                     local v_int_d_queue = buf(base, INT_d_queue_len)
                     t:add(int_d_queue, v_int_d_queue)
                     base = base + INT_d_queue_len
                 end
 
-                if (bit32.band(switch_mapInfo, bit32.lshift(bit_one, 9))) then
+                if (bit32.band(switch_mapInfo, bit32.lshift(byte_one, 9)) ~= byte_zero) then
                     local v_int_d_fwd_acts = buf(base, INT_d_fwd_acts_len)
                     t:add(int_d_fwd_acts, v_int_d_fwd_acts)
                     base = base + INT_d_fwd_acts_len
