2fca867d23610a1df54fe0b7af6b7b03ce980950
iflytang <iflytang@qq.com>
2020-07-14 20:58:51 +0800

    update env files

--- cmake-build-debug/CMakeFiles/CLionProject.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/CLionProject.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "/home/tsf/CLionProject/c_test/test.c" "/home/tsf/CLionProject/cmake-build-debug/CMakeFiles/CLionProject.dir/c_test/test.c.o"
+  "/home/tsf/CLionProject/socket/c_tcp_cli_sock.c" "/home/tsf/CLionProject/cmake-build-debug/CMakeFiles/CLionProject.dir/socket/c_tcp_cli_sock.c.o"
   )
 set(CMAKE_C_COMPILER_ID "GNU")
 
